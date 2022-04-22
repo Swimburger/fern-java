f69238d0a668bac23c2ccaa5dd9fca474f26b599
Deep Singhvi <dsinghvi@umich.edu>
2022-04-22 16:06:07 -0700

    test is back to fern ir

--- fern-model-codegen/src/test/java/com/fern/model/codegen/ModelGeneratorCliTest.java
+++ fern-model-codegen/src/test/java/com/fern/model/codegen/ModelGeneratorCliTest.java
@@ -7,7 +7,7 @@ public class ModelGeneratorCliTest {
     @Test
     public void test_basic() {
         ModelGeneratorCli.main(
-                "fern-model-codegen/src/test/resources/medplum-ir.json",
-                "medplum-model-test");
+                "fern-model-codegen/src/test/resources/fern-ir.json",
+                "fern-model-test");
     }
 }
