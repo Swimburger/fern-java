70595fe8a031e90dd4fdafce105a03307c19b527
Deep Singhvi <dsinghvi@umich.edu>
2022-05-01 20:08:40 -0700

    dockerized plugin

--- fern-client-cli/init.sh
+++ fern-client-cli/init.sh
@@ -1,5 +1,7 @@
-unzip fern-client-cli.zip
-rm -rf fern-client-cli.zip
-cd fern-client-cli
-java -cp fern-client-cli.jar:lib/* \
-  com.fern.java.client.cli.ClientGeneratorCli ../../api/generated/ir.json ../medplum-java-client com.medplum
+
+
+tar -xvf fern-client-cli.tar
+rm -rf fern-client-cli.tar
+cd ../fern-client-cli-"$VERSION"
+java -cp fern-client-cli-"$VERSION".jar:lib/* \
+  com.fern.java.client.cli.ClientGeneratorCli /input/ir.json /output com.medplum
