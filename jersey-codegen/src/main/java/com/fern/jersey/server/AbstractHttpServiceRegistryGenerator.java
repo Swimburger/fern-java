06962eef1ba049518044ced758b073914f394ce1
Deep Singhvi <dsinghvi@umich.edu>
2022-07-03 14:21:05 -0700

    try now

--- jersey-codegen/src/main/java/com/fern/jersey/server/AbstractHttpServiceRegistryGenerator.java
+++ jersey-codegen/src/main/java/com/fern/jersey/server/AbstractHttpServiceRegistryGenerator.java
@@ -3,7 +3,6 @@ package com.fern.jersey.server;
 import com.fern.codegen.GeneratedFile;
 import com.fern.codegen.GeneratedHttpServiceServer;
 import com.fern.codegen.GeneratorContext;
-import com.fern.codegen.IGeneratedFile;
 import com.fern.codegen.utils.ClassNameUtils.PackageType;
 import com.fern.model.codegen.Generator;
 import com.squareup.javapoet.ClassName;
@@ -25,10 +24,10 @@ public final class AbstractHttpServiceRegistryGenerator extends Generator {
     private final List<GeneratedHttpServiceServer> generatedHttpServiceServers;
 
     public AbstractHttpServiceRegistryGenerator(
-            GeneratorContext generatorContext,
-            List<GeneratedHttpServiceServer> generatedHttpServiceServers) {
+            GeneratorContext generatorContext, List<GeneratedHttpServiceServer> generatedHttpServiceServers) {
         super(generatorContext, PackageType.SERVER);
-        this.abstractServiceRegistryClassName = generatorContext.getClassNameUtils()
+        this.abstractServiceRegistryClassName = generatorContext
+                .getClassNameUtils()
                 .getClassName(
                         ABSTRACT_SERVICE_REGISTRY_CLASSSNAME,
                         Optional.empty(),
@@ -44,14 +43,15 @@ public final class AbstractHttpServiceRegistryGenerator extends Generator {
                 .map(generatedHttpServiceServer -> {
                     ClassName httpServiceClassName = generatedHttpServiceServer.className();
                     String methodName = "register" + httpServiceClassName.simpleName();
-                    MethodSpec registerServiceMethod =  MethodSpec.methodBuilder(methodName)
+                    MethodSpec registerServiceMethod = MethodSpec.methodBuilder(methodName)
                             .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
                             .returns(httpServiceClassName)
                             .build();
-                    serviceRegistryConstructor
-                            .addStatement("$L($N())", JERSEY_REGISTER_METHOD_NAME, registerServiceMethod);
+                    serviceRegistryConstructor.addStatement(
+                            "$L($N())", JERSEY_REGISTER_METHOD_NAME, registerServiceMethod);
                     return registerServiceMethod;
-                }).collect(Collectors.toList());
+                })
+                .collect(Collectors.toList());
         TypeSpec abstractServiceRegistryTypespec = TypeSpec.classBuilder(abstractServiceRegistryClassName)
                 .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
                 .superclass(ClassName.get(ResourceConfig.class))
@@ -59,7 +59,7 @@ public final class AbstractHttpServiceRegistryGenerator extends Generator {
                 .addMethods(serviceRegisterMethods)
                 .build();
         JavaFile abstractServiceRegistryFile = JavaFile.builder(
-                abstractServiceRegistryClassName.packageName(), abstractServiceRegistryTypespec)
+                        abstractServiceRegistryClassName.packageName(), abstractServiceRegistryTypespec)
                 .build();
         return GeneratedFile.builder()
                 .file(abstractServiceRegistryFile)
