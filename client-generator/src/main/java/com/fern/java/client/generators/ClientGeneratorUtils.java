82fa379d4fa6e6398b04768ffed430bab0b213f2
dsinghvi <deep@usebirch.com>
2023-03-31 17:19:35 -0700

    upgrade to ir v11

--- client-generator/src/main/java/com/fern/java/client/generators/ClientGeneratorUtils.java
+++ client-generator/src/main/java/com/fern/java/client/generators/ClientGeneratorUtils.java
@@ -16,12 +16,12 @@
 
 package com.fern.java.client.generators;
 
-import com.fern.ir.v9.model.commons.SubpackageId;
-import com.fern.ir.v9.model.commons.TypeId;
-import com.fern.ir.v9.model.http.HttpEndpoint;
-import com.fern.ir.v9.model.http.HttpService;
-import com.fern.ir.v9.model.ir.IPackage;
-import com.fern.ir.v9.model.ir.Subpackage;
+import com.fern.ir.v11.model.commons.SubpackageId;
+import com.fern.ir.v11.model.commons.TypeId;
+import com.fern.ir.v11.model.http.HttpEndpoint;
+import com.fern.ir.v11.model.http.HttpService;
+import com.fern.ir.v11.model.ir.IPackage;
+import com.fern.ir.v11.model.ir.Subpackage;
 import com.fern.java.client.ClientGeneratorContext;
 import com.fern.java.client.GeneratedClientOptions;
 import com.fern.java.client.GeneratedEnvironmentsClass;
