a5a4d5da4f42d9349362dc93f7ab7e38e60222b3
dsinghvi <deep@usebirch.com>
2023-04-03 13:04:25 -0700

    add test for undiscrminated unions

--- generator-utils/src/main/java/com/fern/java/generators/UndiscriminatedUnionGenerator.java
+++ generator-utils/src/main/java/com/fern/java/generators/UndiscriminatedUnionGenerator.java
@@ -19,7 +19,9 @@ package com.fern.java.generators;
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fern.ir.v12.model.types.UndiscriminatedUnionMember;
 import com.fern.ir.v12.model.types.UndiscriminatedUnionTypeDeclaration;
@@ -27,6 +29,7 @@ import com.fern.java.AbstractGeneratorContext;
 import com.fern.java.ObjectMethodFactory;
 import com.fern.java.ObjectMethodFactory.EqualsMethod;
 import com.fern.java.output.GeneratedJavaFile;
+import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.JavaFile;
@@ -57,6 +60,7 @@ public final class UndiscriminatedUnionGenerator extends AbstractFileGenerator {
     private final UndiscriminatedUnionTypeDeclaration undiscriminatedUnion;
     private final Map<UndiscriminatedUnionMember, TypeName> memberTypeNames;
     private final ClassName visitorClassName;
+    private final ClassName deserializerClassName;
 
     public UndiscriminatedUnionGenerator(
             ClassName className,
@@ -69,18 +73,28 @@ public final class UndiscriminatedUnionGenerator extends AbstractFileGenerator {
                         Function.identity(),
                         member -> generatorContext.getPoetTypeNameMapper().convertToTypeName(true, member.getType())));
         this.visitorClassName = className.nestedClass("Visitor");
+        this.deserializerClassName = className.nestedClass("Deserializer");
     }
 
     @Override
     public GeneratedJavaFile generateFile() {
         EqualsMethod equalsMethod = generateEqualsMethod();
         TypeSpec.Builder unionTypeSpec = TypeSpec.classBuilder(className)
+                .addAnnotation(AnnotationSpec.builder(JsonDeserialize.class)
+                        .addMember("using", "$T.class", deserializerClassName)
+                        .build())
                 .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
                 .addField(VALUE_FIELD_SPEC)
                 .addField(FieldSpec.builder(int.class, TYPE_FIELD_NAME)
                         .addModifiers(Modifier.PRIVATE, Modifier.FINAL)
                         .build())
-                .addMethods(getConstructors())
+                .addMethod(MethodSpec.constructorBuilder()
+                        .addModifiers(Modifier.PRIVATE)
+                        .addParameter(Object.class, VALUE_FIELD_SPEC.name)
+                        .addParameter(int.class, TYPE_FIELD_NAME)
+                        .addStatement("this.$L = $L", VALUE_FIELD_SPEC.name, VALUE_FIELD_SPEC.name)
+                        .addStatement("this.$L = $L", TYPE_FIELD_NAME, TYPE_FIELD_NAME)
+                        .build())
                 .addMethod(getRetriever())
                 .addMethod(getVisitMethod())
                 .addMethod(equalsMethod.getEqualsMethodSpec());
@@ -99,26 +113,12 @@ public final class UndiscriminatedUnionGenerator extends AbstractFileGenerator {
                 .build();
     }
 
-    private List<MethodSpec> getConstructors() {
-        List<MethodSpec> constructors = new ArrayList<>();
-        for (int i = 0; i < undiscriminatedUnion.getMembers().size(); ++i) {
-            UndiscriminatedUnionMember member =
-                    undiscriminatedUnion.getMembers().get(i);
-            constructors.add(MethodSpec.constructorBuilder()
-                    .addModifiers(Modifier.PRIVATE)
-                    .addParameter(memberTypeNames.get(member), VALUE_FIELD_SPEC.name)
-                    .addStatement("this.$L = $L", VALUE_FIELD_SPEC.name, VALUE_FIELD_SPEC.name)
-                    .addStatement("this.$L = $L", TYPE_FIELD_NAME, i)
-                    .build());
-        }
-        return constructors;
-    }
-
     private MethodSpec getRetriever() {
         return MethodSpec.methodBuilder("get")
                 .addAnnotation(JsonValue.class)
                 .addModifiers(Modifier.PUBLIC)
                 .addStatement("return this.$N", VALUE_FIELD_SPEC)
+                .returns(VALUE_FIELD_SPEC.type)
                 .build();
     }
 
@@ -126,6 +126,7 @@ public final class UndiscriminatedUnionGenerator extends AbstractFileGenerator {
         MethodSpec.Builder visitMethod = MethodSpec.methodBuilder(VISIT_METHOD_NAME)
                 .addModifiers(Modifier.PUBLIC)
                 .addParameter(ParameterizedTypeName.get(visitorClassName, VISITOR_RETURN_TYPE), "visitor")
+                .addTypeVariable(VISITOR_RETURN_TYPE)
                 .returns(VISITOR_RETURN_TYPE);
         for (int i = 0; i < undiscriminatedUnion.getMembers().size(); ++i) {
             UndiscriminatedUnionMember member =
@@ -189,16 +190,17 @@ public final class UndiscriminatedUnionGenerator extends AbstractFileGenerator {
             staticFactories.add(MethodSpec.methodBuilder(STATIC_FACTORY_METHOD_NAME)
                     .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
                     .addParameter(memberTypeNames.get(member), VALUE_FIELD_SPEC.name)
-                    .addStatement("this($L)", VALUE_FIELD_SPEC.name)
+                    .addStatement("return new $T($L, $L)", className, VALUE_FIELD_SPEC.name, i)
+                    .returns(className)
                     .build());
         }
         return staticFactories;
     }
 
     private TypeSpec getDeserializer() {
-        TypeSpec.Builder deserializerBuilder = TypeSpec.classBuilder(className.nestedClass("Deserializer"))
+        TypeSpec.Builder deserializerBuilder = TypeSpec.classBuilder(deserializerClassName)
                 .addModifiers(Modifier.STATIC, Modifier.FINAL)
-                .addSuperinterface(ParameterizedTypeName.get(StdDeserializer.class, Object.class))
+                .superclass(ParameterizedTypeName.get(ClassName.get(StdDeserializer.class), className))
                 .addMethod(MethodSpec.constructorBuilder()
                         .addStatement("super($T.class)", className)
                         .build());
@@ -222,21 +224,34 @@ public final class UndiscriminatedUnionGenerator extends AbstractFileGenerator {
                                 "return $L(($T) $L)", STATIC_FACTORY_METHOD_NAME, typeName.box(), VALUE_FIELD_SPEC.name)
                         .endControlFlow();
             } else {
-                deserializeMethod
-                        .beginControlFlow("try")
-                        .addStatement(
-                                "$L($T.$N.convertValue($L, $T.class))",
-                                STATIC_FACTORY_METHOD_NAME,
-                                generatorContext.getPoetClassNameFactory().getObjectMapperClassName(),
-                                ObjectMappersGenerator.JSON_MAPPER_STATIC_FIELD_NAME,
-                                VALUE_FIELD_SPEC.name,
-                                typeName)
-                        .nextControlFlow("catch($T e)", IllegalArgumentException.class)
-                        .endControlFlow();
+                if (member.getType().isContainer()) {
+                    deserializeMethod
+                            .beginControlFlow("try")
+                            .addStatement(
+                                    "return $L($T.$N.convertValue($L, new $T() {}))",
+                                    STATIC_FACTORY_METHOD_NAME,
+                                    generatorContext.getPoetClassNameFactory().getObjectMapperClassName(),
+                                    ObjectMappersGenerator.JSON_MAPPER_STATIC_FIELD_NAME,
+                                    VALUE_FIELD_SPEC.name,
+                                    ParameterizedTypeName.get(ClassName.get(TypeReference.class), typeName))
+                            .nextControlFlow("catch($T e)", IllegalArgumentException.class)
+                            .endControlFlow();
+                } else {
+                    deserializeMethod
+                            .beginControlFlow("try")
+                            .addStatement(
+                                    "return $L($T.$N.convertValue($L, $T.class))",
+                                    STATIC_FACTORY_METHOD_NAME,
+                                    generatorContext.getPoetClassNameFactory().getObjectMapperClassName(),
+                                    ObjectMappersGenerator.JSON_MAPPER_STATIC_FIELD_NAME,
+                                    VALUE_FIELD_SPEC.name,
+                                    typeName)
+                            .nextControlFlow("catch($T e)", IllegalArgumentException.class)
+                            .endControlFlow();
+                }
             }
-
-            deserializeMethod.addStatement("throw new $T($S)", JsonParseException.class, "Failed to deserialize");
         }
+        deserializeMethod.addStatement("throw new $T(p, $S)", JsonParseException.class, "Failed to deserialize");
         return deserializerBuilder.addMethod(deserializeMethod.build()).build();
     }
 }
